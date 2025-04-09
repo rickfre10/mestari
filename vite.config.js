5a27155e0c408aed053a52a882394ae4e7ec6bcc
Ricardo Freire <ricardofreireol@gmail.com>
2025-04-09 08:50:23 -0300

    ci: Add deploy workflow and configure base path

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // ADICIONE ESTA LINHA:
-  base: '/mestari/' // IMPORTANTE: Barras no início e no fim!
+
+  base: '/mestari/' 
 })
