d64c5909334b2eb81879ac81cc9d4f93a97271d9
rickfre10 <ricardofreireol@gmail.com>
2025-04-23 00:42:08 -0300

    fix(i18n): Usa fetch para locales na pasta public

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,14 @@ import { viteStaticCopy } from 'vite-plugin-static-copy'
 export default defineConfig({
   plugins: [
     vue(),
-    viteStaticCopy({
-      targets: [
+    /*viteStaticCopy({
+       targets: [
         {
           src: 'src/locales', // caminho dos arquivos de tradução
           dest: ''            // copia direto para a raiz do dist
         }
       ]
-    })
+    })*/
   ],
   base: '/' // importante para GitHub Pages
 })
